9794d54e2784c24e9d9b23afec5b9802cf31ad23
demo <1602803469@qq.com>
2020-10-29 10:47:51 +0800

    声明变量2

--- src/main/java/com/example/demo/test/HelloWorld.java
+++ src/main/java/com/example/demo/test/HelloWorld.java
@@ -3,6 +3,7 @@ package com.example.demo.test;
 public class HelloWorld {
     public static void main(String[] args) {
         int i = 0;
+        int j = 0;
         System.out.println("HelloWorld");
     }
 }
